fed0cf0420b14c0e2f5d186c1e44de45aff68aa6
Barum Rho <barum.rho@gmail.com>
2012-05-08 00:09:22 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='apush',
-    version='0.0.1',
+    version='0.1.0',
     description='A simple Apple push notification service provider',
     long_description=readme,
     author='Barum Rho',
